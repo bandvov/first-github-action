74b52c9e1cc385acec52df1b5ac08273652d5ff3
Volodymyr Vasylkiv <bandvov@gmail.com>
2023-07-31 20:51:34 +0300

    fix service

--- index.js
+++ index.js
@@ -3,39 +3,44 @@ const ejs = require("ejs")
 const path = require("path")
 const nodemailer = require("nodemailer");
 const app = express()
-
+require("dotenv").config()
 const PORT = 5000
 
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "Gmail",
+  secure: false,
+
   auth: {
     // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-    user: 'REPLACE-WITH-YOUR-ALIAS@YOURDOMAIN.COM',
-    pass: 'REPLACE-WITH-YOUR-GENERATED-PASSWORD'
+    user: 'bandvov@gmail.com',
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
-app.get("/send",async(req, res)=>{
+app.get("/send", async (req, res) => {
 
-    let html = await ejs.renderFile(path.join(__dirname,"/template.ejs"), {user: {
-        name:"Vova"
-    }});
-    
-    
-    await transporter.sendMail({
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: "bar@example.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: html, // html body
-      });
+  let html = await ejs.renderFile(path.join(__dirname, "/template.ejs"), {
+    user: {
+      name: "Vova"
+    }
+  });
 
-    res.send(html)
+
+  await transporter.sendMail({
+    from: '"Fred Foo 👻" <bandvov@gmail.com>', // sender address
+    to: "stadniuk.roman@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: html, // html body
+  });
+
+  res.send(html)
 })
 
-app.listen(PORT,()=>{
-    console.log("Server started on port",PORT);
+app.listen(PORT, () => {
+  console.log("Server started on port", PORT);
 })
